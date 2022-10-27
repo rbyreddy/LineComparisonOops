cbba054ab0d85e79917038204b4aa640dce357bd
Rakesh <byreddyrakesh41@gmail.com>
2022-10-27 11:19:10 +0530

    ADDED: Code for the Welcome Message

---
+++ src/linecomparisonoops/WelcomeMessage.java
@@ -0,0 +1,10 @@
+package linecomparisonoops;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line" +
+                "Comparison Computation" +
+                " Program");
+    }
+}
+
